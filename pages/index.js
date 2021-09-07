ce6b3ceb4f6b95b136bb5cc52f2b872b5b33eafa
alenthalachira1998 <alenthalachira.official@gmail.com>
2021-09-07 13:23:35 +0530

    second

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,13 @@ import MainHome from './sections/MainHome'
 import Head from 'next/head'
 import Navbar from '../components/Navbar';
 import Contact from './sections/Contact';
-// import React, {useState, useEffect} from 'react';
-// import Loading from '../components/Loading';
-
 
 
 const index = () => {
-  // const [isLoading, setIsLoading] = useState(true);
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 2500);
-  // })
+
   return (
     <>
-    {/* {isLoading==true? <Loading/> :  */}
-     {/* <div> */}
+  
     <Head>
     <title>Home</title>
     <link rel='icon' href='/favicon.ico' />
